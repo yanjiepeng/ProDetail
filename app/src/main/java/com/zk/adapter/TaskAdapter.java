b0a66c1983c6a00322388b3fc602b1637ef3d930
yanjiepeng <382878591@qq.com>
2016-05-23 17:49:43 +0800

    2016年5月23日 17:49:38 commit

---
+++ app/src/main/java/com/zk/adapter/TaskAdapter.java
@@ -0,0 +1,105 @@
+package com.zk.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.zk.prodetail.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Created by Administrator on 2016/5/23.
+ * 任务列表的适配器
+ */
+public class TaskAdapter extends BaseAdapter {
+
+
+    private Context context;
+    private LayoutInflater inflater;
+    private List<Map<String,Object>> list;
+
+    public TaskAdapter(Context context , List<Map<String,Object>> list) {
+        this.context = context;
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+            if(convertView == null) {
+                view = inflater.inflate(R.layout.task_list_item, null);
+                TextView tv_id = (TextView) view.findViewById(R.id.tv_current_id);
+                TextView tv_type = (TextView) view.findViewById(R.id.tv_current_type);
+                TextView tv_name = (TextView) view.findViewById(R.id.tv_current_name);
+                TextView tv_work_zone = (TextView) view.findViewById(R.id.tv_current_workzone);
+                TextView tv_status = (TextView) view.findViewById(R.id.tv_current_status);
+                TextView tv_finish_time = (TextView) view.findViewById(R.id.tv_current_finish_time);
+
+                //创建viewholder保存对象
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.tv_id = tv_id;
+                viewHolder.tv_type = tv_type;
+                viewHolder.tv_name = tv_name;
+                viewHolder.tv_work_zone = tv_work_zone;
+                viewHolder.tv_status = tv_status;
+                viewHolder.tv_finish_time = tv_finish_time;
+
+                //保存viewholder
+                view.setTag(viewHolder);
+
+            } else {
+                view = convertView;
+                Log.v("TaskAdapter适配器","复用");
+            }
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        //此处获取数据并设置显示
+
+        Map map = list.get(position & list.size());
+
+        viewHolder.tv_id.setText((String) map.get("id"));
+        viewHolder.tv_type.setText((CharSequence) map.get("type"));
+        viewHolder.tv_name.setText((CharSequence) map.get("name"));
+        viewHolder.tv_work_zone.setText((CharSequence) map.get("workzone"));
+        viewHolder.tv_status.setText((CharSequence) map.get("status"));
+        viewHolder.tv_finish_time.setText((CharSequence) map.get("time"));
+
+
+        return view;
+    }
+
+    class ViewHolder {
+        TextView tv_id;
+        TextView tv_type;
+        TextView tv_name;
+        TextView tv_work_zone;
+        TextView tv_status;
+        TextView tv_finish_time;
+    }
+}
---
+++ app/src/main/java/com/zk/prodetail/MainActivity.java
@@ -0,0 +1,164 @@
+package com.zk.prodetail;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.zk.adapter.TaskAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv_proceed_cut, tv_proceed_check, tv_proceed_carving, tv_proceed_scan, tv_proceed_weld, tv_proceed_save;
+    private ListView lv_current_task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initActionBar();
+        hideSystemUI();
+        initContentWidget();
+        PrepareData();
+
+
+    }
+
+    /**
+     * 此方法用于获取数据
+     *
+     * @return list封装数据
+     */
+    private List PrepareData() {
+
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        map.put("id", "0000001");
+        map.put("type", "产品A");
+        map.put("name", "产品A 工件1");
+        map.put("workzone", "工位1");
+        map.put("status", "生产完成");
+        map.put("time", "2016/05/23 15:37");
+
+        list.add(map);
+
+
+        return list;
+    }
+
+    /**
+     * 初始化主界面控件
+     */
+    private void initContentWidget() {
+        tv_proceed_carving = (TextView) findViewById(R.id.tv_proceed_scan);
+        tv_proceed_cut = (TextView) findViewById(R.id.tv_proceed_cut);
+        tv_proceed_check = (TextView) findViewById(R.id.tv_proceed_check);
+        tv_proceed_scan = (TextView) findViewById(R.id.tv_proceed_scan);
+        tv_proceed_weld = (TextView) findViewById(R.id.tv_proceed_weld);
+        tv_proceed_save = (TextView) findViewById(R.id.tv_proceed_save);
+
+        lv_current_task = (ListView) findViewById(R.id.lv_current_task);
+
+
+        LayoutInflater lif = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View headerView = lif.inflate(R.layout.list_header, lv_current_task, false);
+
+        lv_current_task.addHeaderView(headerView);
+
+        lv_current_task.setAdapter(new TaskAdapter(MainActivity.this, PrepareData()));
+    }
+
+    /**
+     * 初始化actionbar
+     */
+    private void initActionBar() {
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setCustomView(R.layout.titlebar);
+        actionBar.setIcon(R.mipmap.mjlogo);
+
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM
+                | ActionBar.DISPLAY_SHOW_HOME);
+
+    }
+
+    /**
+     * 隐藏系统ui
+     */
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                // remove the following flag for version < API 19
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    /**
+     * 开启View闪烁效果
+     */
+
+    private void startFlick(View view) {
+
+        if (null == view) {
+
+            return;
+
+        }
+
+        Animation alphaAnimation = new AlphaAnimation(1, 0);
+
+        alphaAnimation.setDuration(300);
+
+        alphaAnimation.setInterpolator(new LinearInterpolator());
+
+        alphaAnimation.setRepeatCount(Animation.INFINITE);
+
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        view.startAnimation(alphaAnimation);
+
+    }
+
+    /**
+     * 取消View闪烁效果
+     */
+
+    private void stopFlick(View view) {
+
+        if (null == view) {
+
+            return;
+
+        }
+
+        view.clearAnimation();
+
+    }
+
+}
+
+
+
